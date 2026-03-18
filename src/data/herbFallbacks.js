0f93ccd8bb40eadf35f62b52521973d16039938c
WordsofCreation <165867661+WordsofCreation@users.noreply.github.com>
2026-03-18 11:13:48 -0700

    Add 50 more materia medica herbs

--- src/data/herbFallbacks.js
+++ src/data/herbFallbacks.js
@@ -20,6 +20,7 @@ import { herbBatch18 } from './herbBatch18.js';
 import { herbBatch19 } from './herbBatch19.js';
 import { herbBatch20 } from './herbBatch20.js';
 import { herbBatch21 } from './herbBatch21.js';
+import { herbBatch22 } from './herbBatch22.js';
 
 /**
  * Curated fallback monographs keep SacredSeed resilient if upstream APIs fail
@@ -35,7 +36,7 @@ import { herbBatch21 } from './herbBatch21.js';
  * - safetySummary
  * - herbalCategories
  */
-const fallbackBatches = [herbBatchStarter, herbBatch2, herbBatch3, herbBatch4, herbBatch5, herbBatch6, herbBatch7, herbBatch8, herbBatch9, herbBatch10, herbBatch11, herbBatch12, herbBatch13, herbBatch14, herbBatch15, herbBatch16, herbBatch17, herbBatch18, herbBatch19, herbBatch20, herbBatch21];
+const fallbackBatches = [herbBatchStarter, herbBatch2, herbBatch3, herbBatch4, herbBatch5, herbBatch6, herbBatch7, herbBatch8, herbBatch9, herbBatch10, herbBatch11, herbBatch12, herbBatch13, herbBatch14, herbBatch15, herbBatch16, herbBatch17, herbBatch18, herbBatch19, herbBatch20, herbBatch21, herbBatch22];
 
 function withCategoryTags(herb) {
   const herbalCategories = herbCategoryAssignments[herb.slug] ?? herb.herbalCategories ?? [];
